a91b25864ee611e8a44d427e56df617f07fd858e
vinitlee <vinitlee@gmail.com>
2015-07-28 00:01:45 -0700

    better color locations and reduced content

--- public/javascripts/fade.js
+++ public/javascripts/fade.js
@@ -7,7 +7,13 @@ $(function() {
     $.each($('.content > *'),function(i,e) {
       e = $(e);
       var o = (e.position().top-$(window).scrollTop())/$(window).height();
-      o = Math.bound((1-o)*4-1.5,0,1);
+      o = 1-o;
+      o -= 0.6;
+      o *= 20;
+      // o = o>=0.6?1:0;
+      // o *= 5;
+      // o -= 0.4;
+      o = Math.bound(o,0,1)
       console.log(Math.round(100-o*100));
       e.css('color','hsl(156,100%,'+Math.round(50+o*50)+'%)');
     });
